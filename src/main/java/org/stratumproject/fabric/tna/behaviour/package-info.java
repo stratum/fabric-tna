07f7f9b44c0831fe545e13078f20f2288528adb3
Yi Tseng <yi@opennetworking.org>
2020-06-05 00:17:15 +0800

    Add ONOS pipeconf

---
+++ src/main/java/org/stratumproject/fabric/tna/behaviour/package-info.java
@@ -0,0 +1,7 @@
+// Copyright 2017-present Open Networking Foundation
+// SPDX-License-Identifier: Apache-2.0
+
+/**
+ * Fabric-TNA pipeconf behaviour implementations.
+ */
+package org.stratumproject.fabric.tna.behaviour;
---
+++ src/main/java/org/stratumproject/fabric/tna/behaviour/pipeliner/package-info.java
@@ -0,0 +1,7 @@
+// Copyright 2017-present Open Networking Foundation
+// SPDX-License-Identifier: Apache-2.0
+
+/**
+ * Pipeliner implementation classes for fabric.p4.
+ */
+package org.stratumproject.fabric.tna.behaviour.pipeliner;
