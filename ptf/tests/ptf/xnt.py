b2dcab600af09b4794205b6b5e25428e5f98cde3
Yi Tseng <yi@opennetworking.org>
2020-11-20 11:10:17 -0800

    Cleanup Python coding style (#128)
    * Cleanup Python coding style
    
    * fix black rule and constant generator
    
    * fix the dataplane_instance
    
    * Move to PR base prettifier
    
    * try to trigger the prettifier
    
    * fix license
    
    * update PR action version and parameter
    
    * remove debug config
    
    * remove "Test only"
    
    * Update .github/workflows/prettify.yml
    
    Co-authored-by: Carmelo Cascone <carmelo@opennetworking.org>

--- ptf/tests/ptf/xnt.py
+++ ptf/tests/ptf/xnt.py
@@ -3,57 +3,67 @@
 
 # eXtensible Network Telemetry
 
-from scapy.fields import *
-from scapy.packet import *
+from scapy.fields import BitField, ShortField, XByteField, XIntField, XShortField
+from scapy.packet import Packet
 
 
 class INT_META_HDR(Packet):
     name = "INT_META"
-    fields_desc = [BitField("ver", 0, 4),
-                   BitField("rep", 0, 2),
-                   BitField("c", 0, 1),
-                   BitField("e", 0, 1),
-                   BitField("rsvd1", 0, 3),
-                   BitField("ins_cnt", 0, 5),
-                   BitField("max_hop_cnt", 32, 8),
-                   BitField("total_hop_cnt", 0, 8),
-                   ShortField("inst_mask", 0),
-                   ShortField("rsvd2", 0x0000)]
+    fields_desc = [
+        BitField("ver", 0, 4),
+        BitField("rep", 0, 2),
+        BitField("c", 0, 1),
+        BitField("e", 0, 1),
+        BitField("rsvd1", 0, 3),
+        BitField("ins_cnt", 0, 5),
+        BitField("max_hop_cnt", 32, 8),
+        BitField("total_hop_cnt", 0, 8),
+        ShortField("inst_mask", 0),
+        ShortField("rsvd2", 0x0000),
+    ]
 
 
 class INT_L45_HEAD(Packet):
     name = "INT_L45_HEAD"
-    fields_desc = [XByteField("int_type", 0x01),
-                   XByteField("rsvd0", 0x00),
-                   XByteField("length", 0x00),
-                   XByteField("rsvd1", 0x00)]
+    fields_desc = [
+        XByteField("int_type", 0x01),
+        XByteField("rsvd0", 0x00),
+        XByteField("length", 0x00),
+        XByteField("rsvd1", 0x00),
+    ]
 
 
 class INT_L45_TAIL(Packet):
     name = "INT_L45_TAIL"
-    fields_desc = [XByteField("next_proto", 0x01),
-                   XShortField("proto_param", 0x0000),
-                   XByteField("rsvd", 0x00)]
+    fields_desc = [
+        XByteField("next_proto", 0x01),
+        XShortField("proto_param", 0x0000),
+        XByteField("rsvd", 0x00),
+    ]
 
 
 class INT_L45_REPORT_FIXED(Packet):
     name = "INT_L45_REPORT_FIXED"
-    fields_desc = [BitField("ver", 0, 4),
-                   BitField("nproto", 0, 4),
-                   BitField("d", 0, 1),
-                   BitField("q", 0, 1),
-                   BitField("f", 0, 1),
-                   BitField("rsvd", 0, 15),
-                   BitField("hw_id", 1, 6),
-                   XIntField("seq_no", 0),
-                   XIntField("ingress_tstamp", 0)]
+    fields_desc = [
+        BitField("ver", 0, 4),
+        BitField("nproto", 0, 4),
+        BitField("d", 0, 1),
+        BitField("q", 0, 1),
+        BitField("f", 0, 1),
+        BitField("rsvd", 0, 15),
+        BitField("hw_id", 1, 6),
+        XIntField("seq_no", 0),
+        XIntField("ingress_tstamp", 0),
+    ]
 
 
 class INT_L45_LOCAL_REPORT(Packet):
     name = "INT_L45_LOCAL_REPORT"
-    fields_desc = [XIntField("switch_id", 0),
-                   XShortField("ingress_port_id", 0),
-                   XShortField("egress_port_id", 0),
-                   BitField("queue_id", 0, 8),
-                   BitField("queue_occupancy", 0, 24),
-                   XIntField("egress_tstamp", 0)]
+    fields_desc = [
+        XIntField("switch_id", 0),
+        XShortField("ingress_port_id", 0),
+        XShortField("egress_port_id", 0),
+        BitField("queue_id", 0, 8),
+        BitField("queue_occupancy", 0, 24),
+        XIntField("egress_tstamp", 0),
+    ]
