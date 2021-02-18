ee3554db38f5d0c9e18e4b709661f857c48cba10
Yi Tseng <yi@opennetworking.org>
2021-02-18 18:05:19 +0000

    fix bridged metadata size in fabric test

--- ptf/tests/ptf/fabric_test.py
+++ ptf/tests/ptf/fabric_test.py
@@ -196,11 +196,11 @@ BRIDGED_MD_TYPE_EGRESS_MIRROR = 2
 if testutils.test_param_get("profile") == "fabric-spgw-int":
     BMD_BYTES = 51
 elif testutils.test_param_get("profile") == "fabric-spgw":
-    BMD_BYTES = 50
+    BMD_BYTES = 49
 elif testutils.test_param_get("profile") == "fabric-int":
     BMD_BYTES = 29
 else:
-    BMD_BYTES = 28  # fabric
+    BMD_BYTES = 27  # fabric
 IP_HDR_BYTES = 20
 UDP_HDR_BYTES = 8
 GTP_HDR_BYTES = 8
