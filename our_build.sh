458577f879a051c732a5517c3ab19762102b78e1
Robert MacDavid <robert@opennetworking.org>
2021-02-26 12:22:26 -0500

    remove now-unneeded explicit image tags

--- our_build.sh
+++ our_build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-export SDE_DOCKER_IMG=opennetworking/bf-sde:9.3.1
 make fabric-conquest
 
--- our_check.sh
+++ our_check.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-export STRATUM_BF_DOCKER_IM=stratumproject/stratum-bfrt:20.12-9.3.1
-export SDE_DOCKER_IMG=opennetworking/bf-sde:9.3.1
-
 ./ptf/run/tm/run fabric-conquest $@
