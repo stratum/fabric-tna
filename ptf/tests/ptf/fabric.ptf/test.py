e0a59f9ddb9f2d9cf0db7ec8e5f63162b85af384
Yi Tseng <yi@opennetworking.org>
2021-03-16 18:38:50 +0000

    also add IPv4UnicastGroupTestAllPortL4DstIp

--- ptf/tests/ptf/fabric.ptf/test.py
+++ ptf/tests/ptf/fabric.ptf/test.py
@@ -664,6 +664,7 @@ class FabricIPv4UnicastGroupTestAllPortIpDst(FabricTest):
     def runTest(self):
         self.IPv4UnicastGroupTestAllPortL4DstIp("tcp")
         self.IPv4UnicastGroupTestAllPortL4DstIp("udp")
+        self.IPv4UnicastGroupTestAllPortL4DstIp("icmp")
 
 
 class FabricIPv4MPLSTest(FabricTest):
