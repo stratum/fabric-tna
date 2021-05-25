f059694b2f339b6376940d9e2e4b1cd09480d61d
Charles Chan <charles@opennetworking.org>
2021-05-25 10:50:16 -0700

    Fix license

--- src/test/java/org/stratumproject/fabric/tna/stats/HighlightManagerTest.java
+++ src/test/java/org/stratumproject/fabric/tna/stats/HighlightManagerTest.java
@@ -1,3 +1,5 @@
+// Copyright 2021-present Open Networking Foundation
+// SPDX-License-Identifier: LicenseRef-ONF-Member-Only-1.0
 package org.stratumproject.fabric.tna.stats;
 
 import junit.framework.TestCase;
