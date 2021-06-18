5dc6523f7d830f00b5a1ba2d09a899f439e4ade5
Daniele Moro <daniele.moro92@gmail.com>
2021-06-18 23:03:24 +0200

    [SDFAB-189] Add UPF programmable behaviour (#276)
    * Initial move of upf programmable
    
    * Cleanup tests for upf programmable
    
    * Fix checkstyle
    
    * Fix pom file
    
    * Add UPF behaviour when pipeconf requires it
    
    Also, use do not manage UE limit on physical pipeline.
    
    * Force to use local artifacs
    
    * Fix read all counters usage and new tests
    
    * Update full profile suffix.
    
    * Make mocks more generic and independent from upf constants
    
    * Add dependency to fabric v1model and remove redundant interfaces
    
    * Add missing license headers
    
    * Remove dependency to fabric-v1model
    
    * Use different name for distributed structures
    
    * Rename upf store interface name
    
    * Build using snapshots
    
    * Fix MockReadRequest
    
    * Fix failure to find onos-build-conf
    
    Co-authored-by: Carmelo Cascone <carmelo@opennetworking.org>

---
+++ src/test/java/org/stratumproject/fabric/tna/behaviour/upf/MockCounterModel.java
@@ -0,0 +1,43 @@
+// Copyright 2020-present Open Networking Foundation
+// SPDX-License-Identifier: LicenseRef-ONF-Member-Only-1.0
+package org.stratumproject.fabric.tna.behaviour.upf;
+
+import org.onosproject.net.pi.model.PiCounterId;
+import org.onosproject.net.pi.model.PiCounterModel;
+import org.onosproject.net.pi.model.PiCounterType;
+import org.onosproject.net.pi.model.PiTableId;
+
+public class MockCounterModel implements PiCounterModel {
+    PiCounterId id;
+    int size;
+
+    public MockCounterModel(PiCounterId id, int size) {
+        this.id = id;
+        this.size = size;
+    }
+
+    @Override
+    public PiCounterId id() {
+        return this.id;
+    }
+
+    @Override
+    public PiCounterType counterType() {
+        return null;
+    }
+
+    @Override
+    public Unit unit() {
+        return null;
+    }
+
+    @Override
+    public PiTableId table() {
+        return null;
+    }
+
+    @Override
+    public long size() {
+        return this.size;
+    }
+}
---
+++ src/test/java/org/stratumproject/fabric/tna/behaviour/upf/MockPiPipelineModel.java
@@ -0,0 +1,93 @@
+// Copyright 2020-present Open Networking Foundation
+// SPDX-License-Identifier: LicenseRef-ONF-Member-Only-1.0
+package org.stratumproject.fabric.tna.behaviour.upf;
+
+import com.google.common.collect.Maps;
+import org.onosproject.net.pi.model.PiActionProfileId;
+import org.onosproject.net.pi.model.PiActionProfileModel;
+import org.onosproject.net.pi.model.PiCounterId;
+import org.onosproject.net.pi.model.PiCounterModel;
+import org.onosproject.net.pi.model.PiMeterId;
+import org.onosproject.net.pi.model.PiMeterModel;
+import org.onosproject.net.pi.model.PiPacketOperationModel;
+import org.onosproject.net.pi.model.PiPacketOperationType;
+import org.onosproject.net.pi.model.PiPipelineModel;
+import org.onosproject.net.pi.model.PiRegisterId;
+import org.onosproject.net.pi.model.PiRegisterModel;
+import org.onosproject.net.pi.model.PiTableId;
+import org.onosproject.net.pi.model.PiTableModel;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+
+public class MockPiPipelineModel implements PiPipelineModel {
+
+    private final Map<PiTableId, PiTableModel> tableMap = Maps.newHashMap();
+
+    private final List<PiCounterModel> counters;
+
+    public MockPiPipelineModel(Collection<PiTableModel> tables, Collection<PiCounterModel> counters) {
+        tables.forEach(tableModel -> tableMap.put(tableModel.id(), tableModel));
+        this.counters = List.copyOf(counters);
+    }
+
+    @Override
+    public Optional<PiTableModel> table(PiTableId tableId) {
+        return Optional.ofNullable(tableMap.getOrDefault(tableId, null));
+    }
+
+    @Override
+    public Collection<PiTableModel> tables() {
+        return tableMap.values();
+    }
+
+    @Override
+    public Optional<PiCounterModel> counter(PiCounterId counterId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Collection<PiCounterModel> counters() {
+        return counters;
+    }
+
+    @Override
+    public Optional<PiMeterModel> meter(PiMeterId meterId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Collection<PiMeterModel> meters() {
+        return null;
+    }
+
+    @Override
+    public Optional<PiRegisterModel> register(PiRegisterId registerId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Collection<PiRegisterModel> registers() {
+        return null;
+    }
+
+    @Override
+    public Optional<PiActionProfileModel> actionProfiles(PiActionProfileId actionProfileId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Collection<PiActionProfileModel> actionProfiles() {
+        return null;
+    }
+
+    @Override
+    public Optional<PiPacketOperationModel> packetOperationModel(PiPacketOperationType type) {
+        return Optional.empty();
+    }
+
+
+}
