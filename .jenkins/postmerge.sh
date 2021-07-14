2ec07a71b2a1b698e349711f6f216c9572f500ed
Darius Grassi <darius@opennetworking.org>
2021-07-14 15:04:04 -0700

    Implement Yi suggestions * Revert test Docker image name env variable * Clean postmerge code just to push new image * Build local image with reverted name in pr_verify * Add newline to end of ptf/Dockerfile

--- .jenkins/postmerge.sh
+++ .jenkins/postmerge.sh
@@ -17,8 +17,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # requests.
 bash "${DIR}"/pr_verify.sh
 
-# Push local test image to remote cache and stratumproject
-docker tag fabric-tna:ptf registry.opennetworking.org/docker.io/stratumproject/fabric-tna:ptf
-docker push registry.opennetworking.org/docker.io/stratumproject/fabric-tna:ptf
-docker tag fabric-tna:ptf stratumproject/fabric-tna:ptf
-docker push stratumproject/fabric-tna:ptf
+docker push stratumproject/testvectors:ptf
