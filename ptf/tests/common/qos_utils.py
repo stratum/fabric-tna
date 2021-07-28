676677b12311b0e8347df4b0786348100e782cc0
Maximilian Pudelko <pudelkoM@users.noreply.github.com>
2021-07-27 21:16:30 -0700

    Format QoS related files with black and isort (#320)
    This should minimize later additions and the auto prettyfier PR. Formatting-only change.

--- ptf/tests/common/qos_utils.py
+++ ptf/tests/common/qos_utils.py
@@ -1,7 +1,8 @@
 # SPDX-FileCopyrightText: Copyright 2021-present Open Networking Foundation.
 # SPDX-License-Identifier: Apache-2.0
-import logging
 import collections
+import logging
+
 import ptf.testutils as testutils
 from scapy.layers.all import IP, TCP, UDP, Ether
 
@@ -25,18 +26,32 @@ L4_DPORT_CONTROL_TRAFFIC = 1002
 
 # Returns a packet that belongs to the control CoS group.
 def get_control_traffic_packet(l2_size=64):
-    pkt = testutils.simple_udp_packet(eth_dst=DEST_MAC, udp_dport=L4_DPORT_CONTROL_TRAFFIC, pktlen=l2_size)
-    assert(len(pkt) == l2_size), "Packet size {} does not match target size {}".format(len(pkt), l2_size)
+    pkt = testutils.simple_udp_packet(
+        eth_dst=DEST_MAC, udp_dport=L4_DPORT_CONTROL_TRAFFIC, pktlen=l2_size
+    )
+    assert len(pkt) == l2_size, "Packet size {} does not match target size {}".format(
+        len(pkt), l2_size
+    )
     return pkt
+
 
 # Returns a packet that belongs to the system CoS group.
 def get_system_traffic_packet(l2_size=64):
-    pkt = testutils.simple_udp_packet(eth_dst=DEST_MAC, udp_dport=L4_DPORT_SYSTEM_TRAFFIC, pktlen=l2_size)
-    assert(len(pkt) == l2_size), "Packet size {} does not match target size {}".format(len(pkt), l2_size)
+    pkt = testutils.simple_udp_packet(
+        eth_dst=DEST_MAC, udp_dport=L4_DPORT_SYSTEM_TRAFFIC, pktlen=l2_size
+    )
+    assert len(pkt) == l2_size, "Packet size {} does not match target size {}".format(
+        len(pkt), l2_size
+    )
     return pkt
+
 
 # Returns a packet that belongs to the best-effort CoS group.
 def get_best_effort_traffic_packet(l2_size=1400):
-    pkt = testutils.simple_udp_packet(eth_dst=DEST_MAC, udp_dport=L4_DPORT_BEST_EFFORT_TRAFFIC, pktlen=l2_size)
-    assert(len(pkt) == l2_size), "Packet size {} does not match target size {}".format(len(pkt), l2_size)
+    pkt = testutils.simple_udp_packet(
+        eth_dst=DEST_MAC, udp_dport=L4_DPORT_BEST_EFFORT_TRAFFIC, pktlen=l2_size
+    )
+    assert len(pkt) == l2_size, "Packet size {} does not match target size {}".format(
+        len(pkt), l2_size
+    )
     return pkt
--- ptf/tests/common/trex_utils.py
+++ ptf/tests/common/trex_utils.py
@@ -1,13 +1,14 @@
 # SPDX-FileCopyrightText: Copyright 2020-present Open Networking Foundation.
 # SPDX-License-Identifier: Apache-2.0
 import argparse
-import logging
 import collections
+import logging
 
 # Multiplier for data rates
 K = 1000
 M = 1000 * K
 G = 1000 * M
+
 
 def to_readable(src: int, unit: str = "bps") -> str:
     """
@@ -99,60 +100,75 @@ def list_port_status(port_status: dict) -> None:
         readable_stats = get_readable_port_stats(port_status[port])
         logging.info("States from port {}: \n{}".format(port, readable_stats))
 
-LatencyStats = collections.namedtuple("LatencyStats", [
-    "pg_id",
-    "jitter",
-    "average",
-    "total_max",
-    "total_min",
-    "last_max",
-    "histogram",
-    "dropped",
-    "out_of_order",
-    "duplicate",
-    "seq_too_high",
-    "seq_too_low",
-])
 
-FlowStats = collections.namedtuple("FlowStats", [
-    "pg_id",
-    "total_tx",
-    "total_rx",
-    "tx_bytes",
-    "rx_bytes",
-])
+LatencyStats = collections.namedtuple(
+    "LatencyStats",
+    [
+        "pg_id",
+        "jitter",
+        "average",
+        "total_max",
+        "total_min",
+        "last_max",
+        "histogram",
+        "dropped",
+        "out_of_order",
+        "duplicate",
+        "seq_too_high",
+        "seq_too_low",
+    ],
+)
+
+FlowStats = collections.namedtuple(
+    "FlowStats",
+    [
+        "pg_id",
+        "total_tx",
+        "total_rx",
+        "tx_bytes",
+        "rx_bytes",
+    ],
+)
+
 
 def get_latency_stats(pg_id: int, stats) -> LatencyStats:
-    lat_stats = stats['latency'].get(pg_id)
-    lat = lat_stats['latency']
+    lat_stats = stats["latency"].get(pg_id)
+    lat = lat_stats["latency"]
     ret = LatencyStats(
-        pg_id = pg_id,
-        jitter = lat['jitter'],
-        average = lat['average'],
-        total_max = lat['total_max'],
-        total_min = lat['total_min'],
-        last_max = lat['last_max'],
-        histogram = lat['histogram'],
-        dropped = lat_stats['err_cntrs']['dropped'],
-        out_of_order = lat_stats['err_cntrs']['out_of_order'],
-        duplicate = lat_stats['err_cntrs']['dup'],
-        seq_too_high = lat_stats['err_cntrs']['seq_too_high'],
-        seq_too_low = lat_stats['err_cntrs']['seq_too_low'],
+        pg_id=pg_id,
+        jitter=lat["jitter"],
+        average=lat["average"],
+        total_max=lat["total_max"],
+        total_min=lat["total_min"],
+        last_max=lat["last_max"],
+        histogram=lat["histogram"],
+        dropped=lat_stats["err_cntrs"]["dropped"],
+        out_of_order=lat_stats["err_cntrs"]["out_of_order"],
+        duplicate=lat_stats["err_cntrs"]["dup"],
+        seq_too_high=lat_stats["err_cntrs"]["seq_too_high"],
+        seq_too_low=lat_stats["err_cntrs"]["seq_too_low"],
     )
     return ret
 
+
 def get_readable_latency_stats(stats: LatencyStats) -> str:
     histogram = ""
-    l = list(stats.histogram.keys()) # need to listify in order to be able to sort them.
+    # need to listify in order to be able to sort them.
+    l = list(stats.histogram.keys())
     l.sort()
     for sample in l:
         range_start = sample
         if range_start == 0:
             range_end = 10
         else:
-            range_end  = range_start + pow(10, (len(str(range_start))-1))
+            range_end = range_start + pow(10, (len(str(range_start)) - 1))
         val = stats.histogram[sample]
-        histogram = histogram + "\n        Packets with latency between {0:>4} us and {1:>4} us: {2:>10}".format(range_start, range_end, val)
+        histogram = (
+            histogram
+            + "\n        Packets with latency between {0:>4} us and {1:>4} us: {2:>10}".format(
+                range_start, range_end, val
+            )
+        )
 
     return f"""
     Latency info for pg_id {stats.pg_id}
@@ -168,16 +184,18 @@ def get_readable_latency_stats(stats: LatencyStats) -> str:
     Latency distribution histogram: {histogram}
     """
 
+
 def get_flow_stats(pg_id: int, stats) -> FlowStats:
     flow_stats = stats["flow_stats"].get(pg_id)
     ret = FlowStats(
-        pg_id = pg_id,
-        total_tx = flow_stats['tx_pkts']['total'],
-        total_rx = flow_stats['rx_pkts']['total'],
-        tx_bytes = flow_stats['tx_bytes']['total'],
-        rx_bytes = flow_stats['rx_bytes']['total'],
+        pg_id=pg_id,
+        total_tx=flow_stats["tx_pkts"]["total"],
+        total_rx=flow_stats["rx_pkts"]["total"],
+        tx_bytes=flow_stats["tx_bytes"]["total"],
+        rx_bytes=flow_stats["rx_bytes"]["total"],
     )
     return ret
+
 
 class ParseExtendArgAction(argparse.Action):
     def __init__(self, option_strings, dest, nargs=None, **kwargs):
