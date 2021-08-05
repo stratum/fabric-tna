b6b16358cb83b5b1a8f57c06ffbb6e6687a5676a
Darius <dariusgrassi@gmail.com>
2021-08-05 15:40:00 -0500

    Update ptf/tests/linerate/dummy_test.py
    Co-authored-by: Yi Tseng <a86487817@gmail.com>

--- ptf/tests/linerate/dummy_test.py
+++ ptf/tests/linerate/dummy_test.py
@@ -1,15 +1,14 @@
 # Copyright 2020-present Open Networking Foundation
 # SPDX-License-Identifier: LicenseRef-ONF-Member-Only-1.0
 
-from base_test import *
 from ptf.testutils import group
+from ptf.base_tests import BaseTest
 
 @group("spgw")
 @group("bng")
 @group("dth")
 @group("p4rt")
 @group("int-dod")
-class DummyTest():
-    @autocleanup
+class DummyTest(BaseTest):
     def runTest(self):
         pass
