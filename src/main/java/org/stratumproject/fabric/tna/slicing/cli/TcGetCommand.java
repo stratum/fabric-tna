1e08179e9a93c827e8a6a88645848e233495903f
Charles Chan <charles@opennetworking.org>
2021-08-15 21:26:15 -0700

    Implement CLI commands

---
+++ src/main/java/org/stratumproject/fabric/tna/slicing/cli/TcGetCommand.java
@@ -0,0 +1,28 @@
+// Copyright 2021-present Open Networking Foundation
+// SPDX-License-Identifier: LicenseRef-ONF-Member-Only-1.0
+package org.stratumproject.fabric.tna.slicing.cli;
+
+import org.apache.karaf.shell.api.action.Argument;
+import org.apache.karaf.shell.api.action.Command;
+import org.apache.karaf.shell.api.action.lifecycle.Service;
+import org.onosproject.cli.AbstractShellCommand;
+import org.stratumproject.fabric.tna.slicing.api.SliceId;
+import org.stratumproject.fabric.tna.slicing.api.SlicingService;
+
+/**
+ * Get traffic class.
+ */
+@Service
+@Command(scope = "slicing", name = "tc-remove", description = "Get traffic class")
+public class TcGetCommand extends AbstractShellCommand {
+    @Argument(index = 0, name = "sliceId",
+            description = "sliceId. Used to identify a slice.",
+            required = true, multiValued = false)
+    int sliceId;
+
+    @Override
+    protected void doExecute() {
+        SlicingService slicingService = getService(SlicingService.class);
+        print(slicingService.getTrafficClasses(SliceId.of(sliceId)).toString());
+    }
+}
---
+++ src/main/java/org/stratumproject/fabric/tna/slicing/cli/package-info.java
@@ -0,0 +1,7 @@
+// Copyright 2021-present Open Networking Foundation
+// SPDX-License-Identifier: LicenseRef-ONF-Member-Only-1.0
+
+/**
+ * Package for network slicing and QoS CLI.
+ */
+package org.stratumproject.fabric.tna.slicing.cli;
