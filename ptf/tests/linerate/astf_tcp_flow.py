d23ced695d9613363972515ce827c7ae8ecf3ae2
Darius Grassi <darius@opennetworking.org>
2021-09-07 16:30:22 -0700

    Add update from debugging and forum advice * astf_tcp_flow: add switch port configuration and capture receive port * http_eflow: match style of http_eflow profile instead of http_eflow2

--- ptf/tests/linerate/astf_tcp_flow.py
+++ ptf/tests/linerate/astf_tcp_flow.py
@@ -2,10 +2,9 @@
 # SPDX-License-Identifier: LicenseRef-ONF-Member-Only-1.0
 
 from datetime import datetime
+from pprint import pprint
 
-import os
 from trex.astf.api import *
-
 from base_test import *
 from fabric_test import *
 from ptf.testutils import group
@@ -13,24 +12,50 @@ from trex_test import TRexAstfTest
 from trex_utils import list_port_status
 from xnt import analyze_report_pcap
 
-
-TRAFFIC_MULT = 1.0
+# Test specs
+TRAFFIC_MULT = 1000.0
 TEST_DURATION = 10
 CAPTURE_LIMIT = 20
 
-SENDER_PORTS = [0]
-RECEIVER_PORTS = [1]
-INT_COLLECTOR_PORTS = [2]
-
+# TCP tunables
 SIZE = 1
 LOOP = 10
 WIN = 32
 MSS = 0
 
+RECEIVER_PORT = [1]
 
-class AstfTcpFlow(TRexAstfTest, SlicingTest):
+class AstfTcpFlow(TRexAstfTest):
+    """
+    TODO: pydoc
+    """
+
+    def _setup_basic_forwarding(self, out_port) -> None:
+        in_ports = [self.port1, self.port2, self.port3, self.port4]
+        for port in set(in_ports + [out_port]):
+            self.setup_port(port, DEFAULT_VLAN, PORT_TYPE_EDGE)
+        for in_port in in_ports:
+            self.add_forwarding_acl_set_output_port(out_port, ig_port=in_port)
+
+    def setup_basic_forwarding_to_1g(self) -> None:
+        # Forwards all traffic to 1G shaped port per chassis config
+        self._setup_basic_forwarding(out_port=self.port2)
+
+    def setup_basic_forwarding_to_40g(self) -> None:
+        # Forwards all traffic to 40G port per chassis config
+        self._setup_basic_forwarding(out_port=self.port1)
+
     @autocleanup
     def runTest(self):
+
+        self.trex_client.reset()
+        self.trex_client.clear_stats()
+
+        self.setup_basic_forwarding_to_1g()
+
+        capture = self.trex_client.start_capture(
+            rx_ports=RECEIVER_PORT, limit=CAPTURE_LIMIT
+        )
 
         passed = True
 
@@ -38,20 +63,22 @@ class AstfTcpFlow(TRexAstfTest, SlicingTest):
             # load ASTF profile
             profile_path = '/fabric-tna/ptf/tests/linerate/http_eflow2.py'
 
-            profile_tunables = {'size': SIZE, 'loop': LOOP, 'win': WIN, 'mss': MSS}
+            profile_tunables = {'size': SIZE, 'loop': LOOP, 'mss': MSS}
             self.trex_client.load_profile(profile = profile_path, tunables = profile_tunables)
 
             print("Injecting with multiplier of '%s' for %s seconds" % (TRAFFIC_MULT, TEST_DURATION))
-            self.trex_client.start(mult = TRAFFIC_MULT, duration = TEST_DURATION)
+            self.trex_client.start(mult = TRAFFIC_MULT, duration = TEST_DURATION, client_mask=1)
 
-            # block until done
             self.trex_client.wait_on_traffic()
 
-            # read the stats after the test
+            output = "/tmp/tcp-recv-port-{}.pcap".format(
+                datetime.now().strftime("%Y%m%d-%H%M%S")
+            )
+            self.trex_client.stop_capture(capture["id"], output)
             stats = self.trex_client.get_stats()
 
             # use this for debug info on all the stats
-            print(stats)
+            pprint(stats)
 
             if self.trex_client.get_warnings():
                 print('\n\n*** test had warnings ****\n\n')
