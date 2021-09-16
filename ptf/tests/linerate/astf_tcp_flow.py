d461149a2af4c504c6fd7b0927af5d0cb778b806
Darius Grassi <darius@opennetworking.org>
2021-09-16 16:56:23 -0700

    Employ multiple methods to increase throughput and change behaviour of TCP profile

--- ptf/tests/linerate/astf_tcp_flow.py
+++ ptf/tests/linerate/astf_tcp_flow.py
@@ -14,52 +14,58 @@ from trex_utils import list_port_status
 from xnt import analyze_report_pcap
 
 # Test specs
-TRAFFIC_MULT = 10.0
-TEST_DURATION = 10
-LATENCY_PKT_RATE = 1000
+TRAFFIC_MULT = 100000.0
+TEST_DURATION = 60
 
 # TCP tunables
-SIZE = 1
-LOOP = 1000
+SIZE = 9000
+LOOP = 90
 MSS = 0
-WIN = 512
-PIPE = 1
 
-RECEIVER_PORT = [1]
+RECEIVER_PORT = 1
+
+http_req = b'GET /3384 HTTP/1.1\r\nHost: 22.0.0.3\r\nConnection: Keep-Alive\r\nUser-Agent: Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)\r\nAccept: */*\r\nAccept-Language: en-us\r\nAccept-Encoding: gzip, deflate, compress\r\n\r\n'
+
 
 class AstfTcpFlow(QosBaseTest, TRexAstfTest):
     """
     TODO: pydoc
     """
 
-    def create_profile(self, size, loop, mss, win, pipe) -> ASTFProfile: 
+    def create_profile(self, size, loop, mss) -> ASTFProfile: 
 
-        payload = '*'*size*1024
+        http_response = 'HTTP/1.1 200 OK\r\nServer: Microsoft-IIS/6.0\r\nContent-Type: text/html\r\nContent-Length: 32000\r\n\r\n<html><pre>'+('*'*size*1024)+'</pre></html>'
 
+        bsize = len(http_response)
+
+        # client commands
         prog_c = ASTFProgram()
-        prog_c.send(payload)
-        prog_c.recv(len(payload) * loop)
+        prog_c.send(http_req)
+        prog_c.recv(bsize*loop)
 
+        # server commands
         prog_s = ASTFProgram()
-        prog_s.recv(len(payload))
-        if pipe:
-            prog_s.set_send_blocking(False)
-        prog_s.set_var("var2",loop-1); # set var to loop-1 (there is another blocking send)
+        prog_s.recv(len(http_req))
+        prog_s.set_var("var2",loop); # set var 0 to 10 
         prog_s.set_label("a:");
-        prog_s.send(payload)
-        prog_s.jmp_nz("var2","a:") # dec var "var2". in case it is *not* zero jump a: 
-        prog_s.set_send_blocking (True) # back to blocking mode 
-        prog_s.send(payload)
+        prog_s.send(http_response)
+        prog_s.jmp_nz("var2","a:") # dec var "var2". in case it is *not* zero jump a:
 
         # Showcase TCP tuning
         info = ASTFGlobalInfo()
         if mss:
           info.tcp.mss = mss
-        info.tcp.initwnd = 2 # start big
+        info.tcp.initwnd = 20  # start big
         info.tcp.no_delay = 1   # to get fast feedback for acks
-        info.tcp.rxbufsize = win*1024  # 1MB window 
-        info.tcp.txbufsize = win*1024  
-        #info.tcp.do_rfc1323 =0
+        info.tcp.rxbufsize = 1024*1024  # 1MB window 
+        info.tcp.txbufsize = 1024*1024  
+        # info.tcp.do_rfc1323 =1
+
+        # Avoid KEEPALIVE timout
+        # keep alive is much longer in sec time 128sec
+        info.tcp.keepinit = 5000
+        info.tcp.keepidle = 5000
+        info.tcp.keepintvl = 5000
 
 
         # define IP generators
@@ -72,7 +78,7 @@ class AstfTcpFlow(QosBaseTest, TRexAstfTest):
         # define template
         dst_port = qos_utils.L4_DPORT_CONTROL_TRAFFIC
         temp_c = ASTFTCPClientTemplate(
-            program=prog_c, port=dst_port, ip_gen=ip_gen, cps=1, limit=1
+            program=prog_c, port=dst_port, ip_gen=ip_gen, cps=1, limit=500000
         )
         assoc = ASTFAssociationRule(port=dst_port)
         temp_s = ASTFTCPServerTemplate(program=prog_s, assoc=assoc)
@@ -90,26 +96,26 @@ class AstfTcpFlow(QosBaseTest, TRexAstfTest):
         return profile
 
     @autocleanup
-    def runTest(self) -> None:
+    def runTest(self):
 
-        # set up bidirectional forwarding for ports 1 and 2 only
+        # Set up bidirectional forwarding for only ports 1 and 2
         self.setup_port(self.port1, DEFAULT_VLAN, PORT_TYPE_EDGE)
         self.setup_port(self.port2, DEFAULT_VLAN, PORT_TYPE_EDGE)
         self.add_forwarding_acl_set_output_port(self.port2, ig_port=self.port1)
         self.add_forwarding_acl_set_output_port(self.port1, ig_port=self.port2)
 
-        # load astf profile
-        profile_tunables = {'size': SIZE, 'loop': LOOP, 'mss': MSS, 'win': WIN, 'pipe': PIPE}
+        # Load astf profile
+        profile_tunables = {'size': SIZE, 'loop': LOOP, 'mss': MSS}
         self.trex_client.load_profile(profile = self.create_profile(**profile_tunables))
+        self.trex_client.clear_stats()
 
-        # generate tcp traffic
+        # Generate TCP traffic
         print("Injecting with multiplier of '%s' for %s seconds" % (TRAFFIC_MULT, TEST_DURATION))
         self.trex_client.start(
-            mult = TRAFFIC_MULT, duration = TEST_DURATION, latency_pps=LATENCY_PKT_RATE, client_mask=3
+            mult = TRAFFIC_MULT, duration = TEST_DURATION, client_mask=3, nc=True
         )
         self.trex_client.wait_on_traffic()
 
-        # print test stats
         stats = self.trex_client.get_stats()
         pprint.pp(stats)
 
@@ -117,8 +123,6 @@ class AstfTcpFlow(QosBaseTest, TRexAstfTest):
         pprint.pp(names)
         tg_stats = self.trex_client.get_traffic_tg_stats(names)
         pprint.pp(tg_stats)
-        l_stats = self.trex_client.get_latency_stats()
-        pprint.pp(l_stats)
 
         if self.trex_client.get_warnings():
             print('\n\n*** test had warnings ****\n\n')
@@ -128,17 +132,12 @@ class AstfTcpFlow(QosBaseTest, TRexAstfTest):
         client_stats = stats['traffic']['client']
         server_stats = stats['traffic']['server']
 
-        # verify results
+        # Verify results
         tcp_client_sent, tcp_server_recv = client_stats.get('tcps_sndbyte', 0), server_stats.get('tcps_rcvbyte', 0)
         tcp_server_sent, tcp_client_recv = server_stats.get('tcps_sndbyte', 0), client_stats.get('tcps_rcvbyte', 0)
 
         udp_client_sent, udp_server_recv = client_stats.get('udps_sndbyte', 0), server_stats.get('udps_rcvbyte', 0)
         udp_server_sent, udp_client_recv = server_stats.get('udps_sndbyte', 0), client_stats.get('udps_rcvbyte', 0)
-
-        print("tcp_client_sent {} == {} tcp_server_recv".format(tcp_client_sent, tcp_server_recv))
-        print("tcp_server_sent {} == {} tcp_client_recv".format(tcp_server_sent, tcp_client_recv))
-        print("udp_client_sent {} == {} udp_server_recv".format(udp_client_sent, udp_server_recv))
-        print("udp_server_sent {} == {} udp_client_recv".format(udp_server_sent, udp_client_recv))
 
         assert (tcp_client_sent == tcp_server_recv), 'Too much TCP drops - clients sent: %s, servers received: %s' % (tcp_client_sent, tcp_server_recv)
         assert (tcp_server_sent == tcp_client_recv), 'Too much TCP drops - servers sent: %s, clients received: %s' % (tcp_server_sent, tcp_client_recv)
