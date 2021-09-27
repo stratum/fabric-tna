faac21314884a2b9838f2abad9cb2aafc7699d21
Em-G <26747011+EmanueleGallone@users.noreply.github.com>
2021-09-27 19:28:44 +0200

    [SDFAB-582][SDFAB-584] Porting PTF run scripts from fabric-p4tests to fabric-tna (#384)
    * Make ptf/run/bmv2/ scripts consistent with ptf/run/tna/
    
    * Add port map for BmV2
    
    * Remove Tofino-model scripts
    
    * Rename --tofino-pipeline-config parameter in --pipeline-config
    
    * Fix license header for ptf/run/bmv2/port_map.veth.json
    
    * Fix tofino-pipeline-config parameter to pipeline-config
    
    * Fix Linter
    
    * Add mn-stratum container's digest
    
    * Remove comments on old docker tester image
    
    * Remove Todo
    
    * Edit help message in --pipeline-config parser argument
    
    * Refactor update_config() making it more generic.
    
    * Refactor build pipeline method
    
    * Refactor and address changes
    
    * Fix license header
    
    * Refactor container name
    
    * Rename Bmv2 docker image
    
    * Address review changes
    
    * Fix license header
    
    * Fix license header

---
+++ ptf/run/bmv2/base.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+# Copyright 2021-present Open Networking Foundation
+# SPDX-License-Identifier: LicenseRef-ONF-Member-Only-1.0
+
+set -eu -o pipefail
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)"
+FABRIC_TNA_ROOT="${DIR}"/../../..
+
+randomNum=${RANDOM}
+
+# shellcheck source=.env
+source "${FABRIC_TNA_ROOT}"/.env
+
+PTF_FILTER=${PTF_FILTER:-}
+STRATUM_DOCKER_FLAG=${STRATUM_DOCKER_FLAG:-}
+
+fabricProfile=$1
+if [ -z "${fabricProfile}" ]; then
+    echo "fabric profile is not set"
+    exit 1
+fi
+
+echo "*** Testing profile '${fabricProfile}'..."
+
+echo "Running for BMV2"
+P4C_OUT=p4src/v1model/build/${fabricProfile}/bmv2
+echo "*** Using P4 compiler output in ${P4C_OUT}..."
+
+# Clean up old logs (if any)
+rm -rf "${DIR}"/log
+mkdir "${DIR}"/log
+
+# stratum_bmv2
+stratumBmv2ImageName=${STRATUM_BMV2_IMG}
+stratumBmv2RunName=stratum-bmv2-${randomNum}
+
+function stop_stratum_bmv2() {
+    set +e
+    echo "*** Stopping ${stratumBmv2ImageName}..."
+    docker stop -t0 "${stratumBmv2RunName}" > /dev/null
+}
+trap stop_stratum_bmv2 EXIT
+
+
+echo "*** Starting ${stratumBmv2RunName}..."
+docker run --name ${stratumBmv2RunName} -d -it --rm --privileged \
+-v "${FABRIC_TNA_ROOT}":/fabric-tna \
+--entrypoint "/fabric-tna/ptf/run/bmv2/stratum_entrypoint.sh" \
+"${stratumBmv2ImageName}"
+sleep 2
---
+++ ptf/run/bmv2/start_test.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+# Copyright 2021-present Open Networking Foundation
+# SPDX-License-Identifier: LicenseRef-ONF-Member-Only-1.0
+
+set -e
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)"
+PTF_ROOT=${DIR}/../..
+TEST_DIR=${PTF_ROOT}/tests/common
+
+err_report() {
+
+    echo "************************************************"
+    echo "SOME PTF TESTS FAILED :("
+    echo "************************************************"
+    exit 1
+}
+
+trap 'err_report' ERR
+cd "${TEST_DIR}"
+
+echo "************************************************"
+echo "STARTING PTF TESTS..."
+echo "************************************************"
+
+# shellcheck disable=SC2068
+make -f "${DIR}"/Makefile ${@}
+
+echo "************************************************"
+echo "ALL PTF TESTS PASSED :)"
+echo "************************************************"
