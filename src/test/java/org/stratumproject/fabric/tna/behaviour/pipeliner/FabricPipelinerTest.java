57d3b3d5fcb160e0cb2e05908c1ea88086b162b3
Em-G <emanuele.gallone@mail.polimi.it>
2021-10-21 11:13:35 +0200

    Refactor constants in FabricPipelinerTest. Now they're imported from Constants.java

--- src/test/java/org/stratumproject/fabric/tna/behaviour/pipeliner/FabricPipelinerTest.java
+++ src/test/java/org/stratumproject/fabric/tna/behaviour/pipeliner/FabricPipelinerTest.java
@@ -51,19 +51,19 @@ import static org.junit.Assert.assertTrue;
 import static org.onosproject.net.group.DefaultGroupBucket.createCloneGroupBucket;
 import static org.stratumproject.fabric.tna.behaviour.Constants.PORT_TYPE_INTERNAL;
 import static org.stratumproject.fabric.tna.behaviour.Constants.ZERO;
+import static org.stratumproject.fabric.tna.behaviour.Constants.RECIRC_PORTS;
+import static org.stratumproject.fabric.tna.behaviour.Constants.PKT_IN_MIRROR_SESSION_ID;
+import static org.stratumproject.fabric.tna.behaviour.Constants.DEFAULT_VLAN;
+import static org.stratumproject.fabric.tna.behaviour.Constants.FWD_MPLS;
+import static org.stratumproject.fabric.tna.behaviour.Constants.FWD_IPV4_ROUTING;
 import static org.stratumproject.fabric.tna.behaviour.FabricUtils.KRYO;
 
 public class FabricPipelinerTest {
 
     private static final ApplicationId APP_ID = TestApplicationId.create("FabricPipelinerTest");
     private static final DeviceId DEVICE_ID = DeviceId.deviceId("device:1");
-    private static final List<Integer> RECIRC_PORTS = List.of(0x44, 0xc4, 0x144, 0x1c4);
     private static final int DEFAULT_FLOW_PRIORITY = 100;
     private static final int CPU_PORT = 320;
-    private static final byte FWD_MPLS = 1;
-    private static final byte FWD_IPV4_ROUTING = 2;
-    private static final int DEFAULT_VLAN = 4094;
-    private static final int PKT_IN_MIRROR_SESSION_ID = 0x210;
 
     private FabricPipeliner pipeliner;
     private FlowRuleService flowRuleService;
