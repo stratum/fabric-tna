f9e2501b86b1255e8a6ea9aec7f85bcf9d0183f5
Emanuele Gallone <26747011+EmanueleGallone@users.noreply.github.com>
2021-11-16 02:54:49 +0100

    Fix Jenkinsfile (#429)
    * Import Jenkinsfile from UP4
    
    Refactor
    
    Refactor Jenkinsfile
    
    Refactor
    
    Refactor from testing job on Jenkins
    
    Refactor from testing job on Jenkins
    
    Working stages for TNA
    
    Refactor using matrix
    
    Fixing shellcheck
    
    Refactor from Jenkins UI
    
    Fix shellcheck on jflint
    
    Specify agent in matrix. Can now define multiple stages
    
    Remove matrix stages
    
    * Refactor
    
    * Read envs from file .env
    
    * Refactor pipeline
    
    * Refactor reading .env file
    
    * Docker push is now made only once in case of postmerge job
    
    * Address review comments
    
    * Address review comments
    
    * Address review comments. Refactor post failure section
    
    * Address review comments. Parameters no longer use default values.
    
    * Fix make env failing due to repository not checked-out
    
    * Add make targets for docker build and docker push
    
    * Address review comments. Parameters are now sanitized before the start of the pipeline section
    
    * Fix docker logins
    
    * Fix credentials bindings not working when using jenkinsfile
    
    * Fix credentials bindings not working when using jenkinsfile
    
    * Remove fixme
    
    * Fix copyright header
    
    * Add newline at end of file
    
    * Fix returns in return
    
    * Fix codecov token credential
    
    * Fix exit 1 not working
    
    * Fix codecov interpolation warning
    
    * Fix pr-verify-fabric-spgw-int make constants not working.
    
    * Add fixme comment
    
    * Refactor error message for git status --porcelain

--- .jenkins/jflint.sh
+++ .jenkins/jflint.sh
@@ -1,18 +1,12 @@
 #!/usr/bin/env bash
 # Copyright 2021-present Open Networking Foundation
 # SPDX-License-Identifier: LicenseRef-ONF-Member-Only-1.0
-
-# jflint.sh - lint for Jenkins declarative pipeline jobs
 #
 # curl commands from: https://jenkins.io/doc/book/pipeline/development/#linter
-
 set -e -u -o pipefail
-
 JENKINS_URL=https://jenkins.onosproject.org/
 JF_LIST=()
-
 JF_FAIL=0
-
 # if no args, and there's a Jenkinsfile in cwd, check it
 if [ -z "$1" ] && [ -f "Jenkinsfile" ] ; then
   JF_LIST+=("Jenkinsfile")
@@ -27,14 +21,11 @@ else
     fi
   done
 fi
-
 # JENKINS_CRUMB is needed if your Jenkins master has CRSF protection enabled as it should
 JENKINS_CRUMB=$(curl -s "$JENKINS_URL/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,\":\",//crumb)")
-
 for target in "${JF_LIST[@]-}"; do
-  echo "Checking: '${target}' (against ${JENKINS_URL})"
+  echo "Checking: '${target}'"
   CURL_OUT=$(curl -s -H "${JENKINS_CRUMB}" -F "jenkinsfile=<${target}" $JENKINS_URL/pipeline-model-converter/validate)
-
   # Jenkins doesn't set a HTTP failure code when validation fails, so check output
   if [[ $CURL_OUT =~ Jenkinsfile\ successfully\ validated ]]
   then
@@ -44,7 +35,5 @@ for target in "${JF_LIST[@]-}"; do
     echo "$CURL_OUT"
     JF_FAIL=1
   fi
-
 done
-
 exit $JF_FAIL
