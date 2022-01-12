71482aea0c939b127f1fe0aa41d848b612239c92
Carmelo Cascone <carmelo@opennetworking.org>
2022-01-11 21:34:52 -0800

    Remove REST API for adding/removing slice/tcs

--- src/main/java/org/stratumproject/fabric/tna/web/SlicingWebResource.java
+++ src/main/java/org/stratumproject/fabric/tna/web/SlicingWebResource.java
@@ -5,7 +5,6 @@ package org.stratumproject.fabric.tna.web;
 
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-
 import org.onosproject.net.flow.TrafficSelector;
 import org.onosproject.rest.AbstractWebResource;
 import org.slf4j.Logger;
@@ -14,19 +13,18 @@ import org.stratumproject.fabric.tna.slicing.api.SliceId;
 import org.stratumproject.fabric.tna.slicing.api.SlicingService;
 import org.stratumproject.fabric.tna.slicing.api.TrafficClass;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Set;
-
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
-import javax.ws.rs.POST;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Set;
 
 import static org.onlab.util.Tools.readTreeFromStream;
 
@@ -59,51 +57,6 @@ public class SlicingWebResource extends AbstractWebResource {
     }
 
     /**
-     * Add a new slice.
-     * Pre-defined slice IDs: Default Slice = 0, Mobile Traffic Slice = 15
-     *
-     * @param sliceId ID of the slice (DEFAULT_SLICE=0, MOBILE_SLICE=15)
-     * @return 200 ok or 400 bad request
-     */
-    @POST
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Path("slice/{sliceId}")
-    public Response addSlice(@PathParam("sliceId") int sliceId) {
-        boolean result = slicingService.addSlice(SliceId.of(sliceId));
-
-        Response response;
-        if (result) {
-            response = Response.ok().build();
-        } else {
-            response = Response.status(400).build();
-        }
-
-        return response;
-    }
-
-    /**
-     * Remove an existing slice.
-     * Pre-defined slice IDs: Default Slice = 0, Mobile Traffic Slice = 15
-     *
-     * @param sliceId ID of the slice
-     * @return 200 ok or 400 bad request
-     */
-    @DELETE
-    @Path("slice/{sliceId}")
-    public Response removeSlice(@PathParam("sliceId") int sliceId) {
-        boolean result = slicingService.removeSlice(SliceId.of(sliceId));
-
-        Response response;
-        if (result) {
-            response = Response.ok().build();
-        } else {
-            response = Response.status(400).build();
-        }
-
-        return response;
-    }
-
-    /**
      * Get all traffic class of a slice.
      * Pre-defined slice IDs: Default Slice = 0, Mobile Traffic Slice = 15
      *
@@ -125,31 +78,6 @@ public class SlicingWebResource extends AbstractWebResource {
         } else {
             response = Response.status(404).build();
         }
-        return response;
-    }
-
-    /**
-     * Add a traffic class to a slice.
-     * Pre-defined slice IDs: Default Slice = 0, Mobile Traffic Slice = 15
-     * Traffic class values: CONTROL, REAL_TIME, ELASTIC, BEST_EFFORT
-     *
-     * @param sliceId ID of the slice
-     * @param tc Traffic class to be added to the given slice
-     * @return 200 ok or 400 bad request
-     */
-    @POST
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Path("tc/{sliceId}/{tc}")
-    public Response addTc(@PathParam("sliceId") int sliceId, @PathParam("tc") String tc) {
-        boolean result = slicingService.addTrafficClass(SliceId.of(sliceId), TrafficClass.valueOf(tc));
-
-        Response response;
-        if (result) {
-            response = Response.ok().build();
-        } else {
-            response = Response.status(400).build();
-        }
-
         return response;
     }
 
@@ -190,29 +118,6 @@ public class SlicingWebResource extends AbstractWebResource {
     @Path("defaulttc/{sliceId}/{tc}")
     public Response setDefaultTc(@PathParam("sliceId") int sliceId, @PathParam("tc") String tc) {
         boolean result = slicingService.setDefaultTrafficClass(SliceId.of(sliceId), TrafficClass.valueOf(tc));
-
-        Response response;
-        if (result) {
-            response = Response.ok().build();
-        } else {
-            response = Response.status(400).build();
-        }
-
-        return response;
-    }
-
-    /**
-     * Remove a traffic class from a slice.
-     * Pre-defined slice IDs: Default Slice = 0, Mobile Traffic Slice = 15
-     *
-     * @param sliceId ID of the slice
-     * @param tc Traffic class to be removed
-     * @return 200 ok or 400 bad request
-     */
-    @DELETE
-    @Path("tc/{sliceId}/{tc}")
-    public Response removeTc(@PathParam("sliceId") int sliceId, @PathParam("tc") String tc) {
-        boolean result = slicingService.removeTrafficClass(SliceId.of(sliceId), TrafficClass.valueOf(tc));
 
         Response response;
         if (result) {
