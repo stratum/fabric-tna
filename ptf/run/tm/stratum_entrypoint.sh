7f97a893ea42378ed251a58738fede6a27f0af6c
Carmelo Cascone <carmelo@opennetworking.org>
2022-02-04 11:18:56 -0800

    Update ptf/run/tm/stratum_entrypoint.sh

--- ptf/run/tm/stratum_entrypoint.sh
+++ ptf/run/tm/stratum_entrypoint.sh
@@ -35,5 +35,5 @@ ${stratumBin} \
     -persistent_config_dir=/tmp \
     -write_req_log_file=./p4rt-write-reqs.log \
     -enable_onlp=false \
-    -enable_bfrt_legacy_bytestring_responses=true \
+    -incompatible_enable_bfrt_legacy_bytestring_responses=true \
     > ./stratum_bf.log 2>&1
