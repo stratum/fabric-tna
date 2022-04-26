a78cf66dc3e3603ae3cc1df06c6991ce0019e5f9
Daniele Moro <daniele.moro@intel.com>
2022-04-26 15:25:00 +0200

    Use ONF copyright on new files

--- src/test/java/org/stratumproject/fabric/tna/behaviour/upf/MockWriteRequest.java
+++ src/test/java/org/stratumproject/fabric/tna/behaviour/upf/MockWriteRequest.java
@@ -1,4 +1,4 @@
-// Copyright 2022-present Intel Corporation
+// Copyright 2022-present Open Networking Foundation
 // SPDX-License-Identifier: Apache-2.0
 
 package org.stratumproject.fabric.tna.behaviour.upf;
